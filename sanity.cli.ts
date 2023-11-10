24a449ff1090806278db69a11a2f5907b9aa5024
thilaksundaram2 <thilak@structlabs.io>
2023-11-10 15:12:03 +0800

    Initial commit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,8 @@
 import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || '';
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || '';
+const dataset =  '';
+
 
 export default defineCliConfig({ api: { projectId, dataset } })
